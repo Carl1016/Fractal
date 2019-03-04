71ed04c52eb2bee263a9ff9e6df6af84aa63f303
Carl1016 <410335011@gms.ndhu.edu.tw>
2019-03-04 16:56:35 +0800

    加入專案檔案。

---
+++ Fractal/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 419);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(356, 419);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 29);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(573, 419);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(3);
+            this.pictureBox1.Size = new System.Drawing.Size(798, 412);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Geometry";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 423);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Depth";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 494);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Fractal/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        int Geometry = 0;
+        int Depth = 0;
+        Pen pen = new Pen(Brushes.Blue);
+        Point A = new Point();
+        Point B = new Point();
+        
+        Pen eraser = new Pen(Brushes.White,3);
+        
+        List<Rectangle> pointset = new List<Rectangle> { };
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text!="" && textBox2.Text!="")
+            {
+                if(textBox2.Text =="0")
+                {
+                    MessageBox.Show("Error: Depth must more than 1");
+                }
+                else
+                {
+                    Geometry = Convert.ToInt32(textBox1.Text);
+                    Depth = Convert.ToInt32(textBox2.Text);
+                    Rectangle rect = new Rectangle(Geometry, Depth, 10, 10);
+                }
+                             
+            }
+            else
+            {
+                MessageBox.Show("Wrong input!");
+            }            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Rectangle rect = new Rectangle(e.X, e.Y, 5, 5);
+            g.FillRectangle(Brushes.Black, rect);
+            pointset.Add(rect);
+            if(pointset.Count==2)
+            {
+                
+                g.DrawLine(pen, pointset[0].Location, pointset[1].Location);
+                A.X = pointset[0].X;
+                A.Y = pointset[0].Y;
+                B.X = pointset[1].X;
+                B.Y = pointset[1].Y;
+                fractal_maker(Geometry, Depth,A,B);
+                pointset.Remove(pointset[0]);
+            }
+            
+        }
+        public void fractal_maker(int Polygon_num,int depth,Point A, Point B)
+        {           
+            Point p = new Point();
+            Point C = new Point();
+            Point D = new Point();
+            C.X = A.X + (B.X - A.X) / 3;
+            C.Y = A.Y + (B.Y - A.Y) / 3;
+            D.X = A.X + (B.X - A.X) / 3 * 2;
+            D.Y = A.Y + (B.Y - A.Y) / 3 * 2;
+            g.DrawLine(eraser, C, D);
+            if(depth==1)
+            {
+                g.DrawLine(pen, A, B);
+            }
+            else
+            {
+                
+                switch (Polygon_num)
+                {
+                    case 3:
+                        p = triangle_maker(C,D,depth);
+                        fractal_maker(Polygon_num, depth - 1, A, C);
+                        fractal_maker(Polygon_num, depth - 1, C, p);
+                        fractal_maker(Polygon_num, depth - 1, p, D);
+                        fractal_maker(Polygon_num, depth - 1, D, B);
+                        break;
+                    case 4:
+                        Point[] points = new Point[2];
+                        points= Rectangle_maker(C, D);
+                        fractal_maker(Polygon_num,depth-1,A, C);
+                        fractal_maker(Polygon_num, depth - 1, C, points[0]);
+                        fractal_maker(Polygon_num, depth - 1, points[0], points[1]);
+                        fractal_maker(Polygon_num, depth - 1, points[1], D);
+                        fractal_maker(Polygon_num, depth - 1, D, B);
+
+                        break;
+                    case 5:
+                        Point[] points1 = new Point[3];
+                        points1 = pentagon_maker(C,D);
+                        fractal_maker(Polygon_num, depth - 1, A, C);
+                        fractal_maker(Polygon_num, depth - 1, C, points1[0]);
+                        fractal_maker(Polygon_num, depth - 1, points1[0], points1[1]);
+                        fractal_maker(Polygon_num, depth - 1, points1[1], points1[2]);
+                        fractal_maker(Polygon_num, depth - 1, points1[2], D);
+                        fractal_maker(Polygon_num, depth - 1, D,B);
+                        break;
+                    case 6:
+                        Point[] points2 = new Point[4];
+                        points2 = hexagon_maker(C,D);
+                        fractal_maker(Polygon_num, depth - 1, A, C);
+                        fractal_maker(Polygon_num, depth - 1, C, points2[0]);
+                        fractal_maker(Polygon_num, depth - 1, points2[0], points2[1]);
+                        fractal_maker(Polygon_num, depth - 1, points2[1], points2[2]);
+                        fractal_maker(Polygon_num, depth - 1, points2[2], points2[3]);
+                        fractal_maker(Polygon_num, depth - 1, points2[3], D);
+                        fractal_maker(Polygon_num, depth - 1, D, B);
+                        break;
+                    default:
+                        Console.WriteLine("wrong input");
+                        break;
+                      
+                }
+                
+
+            }
+
+            
+
+        }
+        public Point triangle_maker(Point C, Point D,int depth)
+        {
+            
+            double x1 = (D.X - C.X) * Math.Cos(-60*Math.PI/180) - (D.Y - C.Y) * Math.Sin(-60*Math.PI/180) + C.X;
+            double y1 = (D.X - C.X) * Math.Sin(-60*Math.PI/180) + (D.Y - C.Y) * Math.Cos(-60*Math.PI/180) + C.Y;
+            Point point = new Point();
+            point.X = (int)x1;
+            point.Y = (int)y1;
+            g.DrawLine(pen, C, point);
+            g.DrawLine(pen, D, point);
+            return point;
+
+        }   
+        public Point[] Rectangle_maker(Point C, Point D)
+        {
+
+            Point[] points = new Point[2];
+            Point point = new Point();
+            double x1 = (D.X - C.X) * Math.Cos(-90 * Math.PI / 180) - (D.Y - C.Y) * Math.Sin(-90 * Math.PI / 180) + C.X;
+            double y1 = (D.X - C.X) * Math.Sin(-90 * Math.PI / 180) + (D.Y - C.Y) * Math.Cos(-90 * Math.PI / 180) + C.Y;
+            point.X = (int)x1;
+            point.Y = (int)y1;
+            g.DrawLine(pen, C, point);
+            Point point1 = new Point();
+            double x2 = (C.X - point.X) * Math.Cos(-90 * Math.PI / 180) - (C.Y - point.Y) * Math.Sin(-90 * Math.PI / 180) + point.X;
+            double y2 = (C.X - point.X) * Math.Sin(-90 * Math.PI / 180) + (C.Y - point.Y) * Math.Cos(-90 * Math.PI / 180) + point.Y;
+            point1.X = (int)x2;
+            point1.Y = (int)y2;
+            g.DrawLine(pen, point, point1);
+            g.DrawLine(pen, point1, D);
+            points[0] = point;
+            points[1] = point1;
+            return points;
+
+        }
+        public Point[] pentagon_maker(Point C, Point D)
+        {
+            Point[] points = new Point[3];
+            Point point = new Point();
+            double x1 = (D.X - C.X) * Math.Cos(-108 * Math.PI / 180) - (D.Y - C.Y) * Math.Sin(-108 * Math.PI / 180) + C.X;
+            double y1 = (D.X - C.X) * Math.Sin(-108 * Math.PI / 180) + (D.Y - C.Y) * Math.Cos(-108 * Math.PI / 180) + C.Y;
+            point.X = (int)x1;
+            point.Y = (int)y1;
+            g.DrawLine(pen, C, point);
+            Point point1 = new Point();
+            double x2 = (C.X - point.X) * Math.Cos(-108 * Math.PI / 180) - (C.Y - point.Y) * Math.Sin(-108 * Math.PI / 180) + point.X;
+            double y2 = (C.X - point.X) * Math.Sin(-108 * Math.PI / 180) + (C.Y - point.Y) * Math.Cos(-108 * Math.PI / 180) + point.Y;
+            point1.X = (int)x2;
+            point1.Y = (int)y2;
+            g.DrawLine(pen, point, point1);
+            Point point2 = new Point();
+            double x3 = (point.X - point1.X) * Math.Cos(-108 * Math.PI / 180) - (point.Y - point1.Y) * Math.Sin(-108 * Math.PI / 180) + point1.X;
+            double y3 = (point.X - point1.X) * Math.Sin(-108 * Math.PI / 180) + (point.Y - point1.Y) * Math.Cos(-108 * Math.PI / 180) + point1.Y;
+            point2.X = (int)x3;
+            point2.Y = (int)y3;
+            g.DrawLine(pen, point1, point2);
+            g.DrawLine(pen, point2, D);
+            points[0] = point;
+            points[1] = point1;
+            points[2] = point2;
+            return points;
+
+        }
+        public Point[] hexagon_maker(Point C, Point D)
+        {
+            Point[] points = new Point[4];
+            Point point = new Point();
+            double x1 = (D.X - C.X) * Math.Cos(-120 * Math.PI / 180) - (D.Y - C.Y) * Math.Sin(-120 * Math.PI / 180) + C.X;
+            double y1 = (D.X - C.X) * Math.Sin(-120 * Math.PI / 180) + (D.Y - C.Y) * Math.Cos(-120 * Math.PI / 180) + C.Y;
+            point.X = (int)x1;
+            point.Y = (int)y1;
+            g.DrawLine(pen, C, point);
+            Point point1 = new Point();
+            double x2 = (C.X - point.X) * Math.Cos(-120 * Math.PI / 180) - (C.Y - point.Y) * Math.Sin(-120 * Math.PI / 180) + point.X;
+            double y2 = (C.X - point.X) * Math.Sin(-120 * Math.PI / 180) + (C.Y - point.Y) * Math.Cos(-120 * Math.PI / 180) + point.Y;
+            point1.X = (int)x2;
+            point1.Y = (int)y2;
+            g.DrawLine(pen, point, point1);
+            Point point2 = new Point();
+            double x3 = (point.X - point1.X) * Math.Cos(-120 * Math.PI / 180) - (point.Y - point1.Y) * Math.Sin(-120 * Math.PI / 180) + point1.X;
+            double y3 = (point.X - point1.X) * Math.Sin(-120 * Math.PI / 180) + (point.Y - point1.Y) * Math.Cos(-120 * Math.PI / 180) + point1.Y;
+            point2.X = (int)x3;
+            point2.Y = (int)y3;
+            g.DrawLine(pen, point1, point2);
+            Point point3 = new Point();
+            double x4 = (point1.X - point2.X) * Math.Cos(-120 * Math.PI / 180) - (point1.Y - point2.Y) * Math.Sin(-120 * Math.PI / 180) + point2.X;
+            double y4 = (point1.X - point2.X) * Math.Sin(-120 * Math.PI / 180) + (point1.Y - point2.Y) * Math.Cos(-120 * Math.PI / 180) + point2.Y;
+            point3.X = (int)x4;
+            point3.Y = (int)y4;
+            g.DrawLine(pen, point2, point3);
+            g.DrawLine(pen, point3, D);
+            points[0] = point;
+            points[1] = point1;
+            points[2] = point2;
+            points[3] = point3;
+            return points;
+
+        }
+
+    }
+}
